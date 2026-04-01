da0c513ee7ed95c28bc34ceea750164237b4782e
Sunghyun Cho <hey@cho.sh>
2026-04-01 15:30:28 +0900

    track WASM pkg for Vercel deployment
    The WASM build output was gitignored, causing Vercel builds to
    fail since Rust/wasm-pack aren't available in the build environment.

---
+++ crates/markright-wasm/pkg/markright_wasm_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const format: (a: number, b: number) => [number, number];
+export const lint: (a: number, b: number) => [number, number];
+export const parse: (a: number, b: number) => [number, number, number, number];
+export const parse_to_html: (a: number, b: number) => [number, number];
+export const schema: () => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
